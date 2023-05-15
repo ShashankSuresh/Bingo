feb1e3d535b102670a4d266710cd6fe221a04f56
ShashankSuresh <shashank.suresh@wundermanthompson.com>
2023-05-15 11:16:03 +0100

    feat: shuffle board when page is refreshed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useMemo } from 'react';
 import Bingo from './pages/Bingo';
+import { shuffleArray } from './utils/utils';
 
 import { buzzwordsData } from './data/data';
 
@@ -10,7 +11,8 @@ const FREE_TEXT = "Free";
 const App: React.FC = () => {
   // Memoizing to reduce re-rendering
   const data = useMemo(() => {
-    const copyOfBuzzwordsData = [...buzzwordsData];
+    const shuffleBuzzwordsData = shuffleArray(buzzwordsData)
+    const copyOfBuzzwordsData = [...shuffleBuzzwordsData];
     const middleIndex = Math.floor(copyOfBuzzwordsData.length / 2);
     // Inserting the text Free in between the grid
     copyOfBuzzwordsData.splice(middleIndex, 0, FREE_TEXT);
